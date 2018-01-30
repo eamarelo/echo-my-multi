757efd302a904a378f7e87172507453f62117a19
eamarelo <eamarelo@hotmail.fr>
2018-01-30 10:05:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const number = 1;
+const limit = 10;
+
+console.log (`\n [Start] ---> Table de ${number}\n`);
+
+for (let i = 1 ; i >= limit; i++) {
+    console.log (`${number} X ${i} = ${(i*limit)}`)
+}
